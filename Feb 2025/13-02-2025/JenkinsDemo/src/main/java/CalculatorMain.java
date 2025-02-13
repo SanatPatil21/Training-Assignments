fc82aa84ab441619c208a0f09cb12ad772224fa6
Sanat Patil <sanat.patil@wissen.com>
2025-02-13 16:53:34 +0530

    Commit for Jenkins 4

--- Feb 2025/13-02-2025/JenkinsDemo/src/main/java/CalculatorMain.java
+++ Feb 2025/13-02-2025/JenkinsDemo/src/main/java/CalculatorMain.java
@@ -13,6 +13,10 @@ class Calculator
 		System.out.println("Multiply : "+(a * b));
 	}
 	//Some Changes
+	public void div(int a,int b)
+	{
+		System.out.println("Divide : "+(a/b));
+	}
 }
 public class CalculatorMain
 {
@@ -24,5 +28,7 @@ public class CalculatorMain
 		c1.add(10, 20);
 		c1.sub(10, 20);
 		c1.mul(10, 20);
+		c1.div(20, 10);
+		
 	}
 }
